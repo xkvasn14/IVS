d90dcd660af5b45e009acef507401cb436836fff
xkvasn14 <61392422+xkvasn14@users.noreply.github.com>
2022-03-08 15:25:34 +0100

    Add files via upload

---
+++ BB,WB,TDD/black_box_tests.cpp
@@ -0,0 +1,217 @@
+//======== Copyright (c) 2017, FIT VUT Brno, All rights reserved. ============//
+//
+// Purpose:     Red-Black Tree - public interface tests
+//
+// $NoKeywords: $ivs_project_1 $black_box_tests.cpp
+// $Author:     Jarolav Kvasnicka <xkvasn14@stud.fit.vutbr.cz>
+// $Date:       $2017-01-04
+//============================================================================//
+/**
+ * @file black_box_tests.cpp
+ * @author Jaroslav Kvasnicka
+ * 
+ * @brief Implementace testu binarniho stromu.
+ */
+
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "red_black_tree.h"
+
+
+class EmptyTree : public BinaryTree, public testing::Test
+{
+    protected:
+        BinaryTree Tree;
+};
+
+class NonEmptyTree : public BinaryTree, public testing::Test
+{
+    protected:   
+        BinaryTree Tree;
+        std::vector<int> vectr = { 1,7,3,4,10 };
+        std::vector<std::pair<bool, Node_t*>> outNewNodes;
+        std::vector<Node_t*> outLeafNodes;
+        std::vector<Node_t*> outAllNodes;
+        std::vector<Node_t*> outNonLeafNodes;
+};
+    
+
+TEST_F(EmptyTree, InsertNode)
+{
+    //insert of node_1
+    std::pair<bool, Node_t*> node_1;
+    node_1 = InsertNode(1);
+    EXPECT_TRUE(node_1.first);
+    EXPECT_NE(node_1.second, nullptr);
+
+    //insert of node_2
+    std::pair<bool, Node_t*> node_2;
+    node_2 = InsertNode(2);
+    EXPECT_NE(node_1.second, node_2.second);
+    EXPECT_TRUE(node_2.first);
+    
+    EXPECT_EQ(node_2.second->pParent->key, 1);
+    EXPECT_EQ(node_1.second->pRight->key, 2);
+
+    //insert of node 3
+    std::pair<bool, Node_t*> node_3;
+    node_3 = InsertNode(3);
+    EXPECT_NE(node_1.second, node_3.second);
+    EXPECT_NE(node_2.second, node_3.second);
+    EXPECT_TRUE(node_3.first);
+
+    EXPECT_EQ(node_3.second->pParent->key, node_2.second->key);
+    EXPECT_EQ(node_3.second->pParent->pLeft->key, node_1.second->key);
+
+}
+
+TEST_F(EmptyTree, DeleteNode)
+{
+    EXPECT_FALSE(DeleteNode(1));
+    std::pair<bool, Node_t*> node_1;
+    node_1 = InsertNode(1);
+    EXPECT_TRUE(DeleteNode(1));
+    EXPECT_FALSE(FindNode(1));
+}
+
+TEST_F(EmptyTree, FindNode)
+{
+    EXPECT_EQ(FindNode(1),nullptr);
+    std::pair<bool, Node_t*> node_1;
+    node_1 = InsertNode(1);
+    EXPECT_NE(FindNode(1), nullptr);
+    EXPECT_EQ(FindNode(1), node_1.second);
+}
+
+/////////////////////////////////////////////////////////////////////
+
+TEST_F(NonEmptyTree, InsertNode)
+{
+    Tree.InsertNodes(vectr, outNewNodes);
+    //TESTING NEW NODE IN TREE
+    std::pair<bool, Node_t*> node_1 = Tree.InsertNode(5);
+    EXPECT_TRUE(node_1.first);
+
+    //TESTING NODE IN TREE
+    std::pair<bool, Node_t*> node_2 = Tree.InsertNode(4);
+    EXPECT_FALSE(node_2.first);
+
+    //TESTING CONNECTION BETWEEN 
+    std::pair<bool, Node_t*> node_3 = Tree.InsertNode(6);
+    EXPECT_TRUE(node_3.first);
+    EXPECT_EQ(node_3.second->pParent->key,node_1.second->key);
+}
+
+TEST_F(NonEmptyTree, DeleteNode)
+{
+    Tree.InsertNodes(vectr, outNewNodes);
+    //INSERTING AND DELETING NODE
+    std::pair<bool, Node_t*> node_1 = Tree.InsertNode(5);
+    //EXPECT_TRUE(Tree.DeleteNode(5));
+
+    for (int i = 0; i < outNewNodes.size(); i++)
+    {
+        //EXPECT_DOUBLE_EQ(i, 9);
+        //EXPECT_DOUBLE_EQ(outNewNodes[i].second->key, 100);
+        EXPECT_TRUE(Tree.DeleteNode(outNewNodes[i].second->key));
+    }
+}
+
+TEST_F(NonEmptyTree, FindNode)
+{
+    Tree.InsertNodes(vectr, outNewNodes);
+    //FINDING BY ARR POSITION
+    EXPECT_NE(Tree.FindNode(outNewNodes[0].second->key),nullptr);
+    EXPECT_NE(Tree.FindNode(outNewNodes[1].second->key),nullptr);
+    EXPECT_NE(Tree.FindNode(outNewNodes[2].second->key),nullptr);
+    EXPECT_NE(Tree.FindNode(outNewNodes[3].second->key),nullptr);
+    EXPECT_NE(Tree.FindNode(outNewNodes[4].second->key),nullptr);
+    EXPECT_DEATH_IF_SUPPORTED(Tree.FindNode(outNewNodes[5].second->key),"");
+
+    //FINDING BY NUMBER
+    EXPECT_NE(Tree.FindNode(1),nullptr);
+    EXPECT_NE(Tree.FindNode(3),nullptr);
+    EXPECT_NE(Tree.FindNode(4),nullptr);
+    EXPECT_NE(Tree.FindNode(7),nullptr);
+    EXPECT_NE(Tree.FindNode(10),nullptr);
+    EXPECT_EQ(Tree.FindNode(2),nullptr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+TEST_F(NonEmptyTree, Axiom1)
+{
+    Tree.InsertNodes(vectr, outNewNodes);
+    Tree.GetLeafNodes(outLeafNodes);
+
+    for (int i = 0; i < outLeafNodes.size(); i++)
+    {
+        EXPECT_TRUE(outLeafNodes[i]->color == Color_t::BLACK);
+    }
+}
+
+TEST_F(NonEmptyTree, Axiom2)
+{
+    Tree.InsertNodes(vectr, outNewNodes);
+    Tree.GetAllNodes(outAllNodes);
+
+    for (int i = 0; i < outAllNodes.size(); i++)
+    {
+        if (outAllNodes[i]->color == Color_t::RED)
+        {
+            EXPECT_TRUE(outAllNodes[i]->pLeft->color == Color_t::BLACK);
+            EXPECT_TRUE(outAllNodes[i]->pRight->color == Color_t::BLACK);
+        }
+    }
+}
+
+TEST_F(NonEmptyTree, Axiom3)
+{
+    Tree.InsertNodes(vectr, outNewNodes);
+    Tree.GetAllNodes(outAllNodes);
+    Tree.GetLeafNodes(outLeafNodes);
+
+    int blacks = 0;
+    int blacks_in_tree[sizeof(outLeafNodes)];
+    int i = 0;
+    Node_t* neznama;
+    
+    for ( i = 0; i < outLeafNodes.size(); i++)
+    {
+        blacks = 0;
+        neznama = outLeafNodes[i];
+        while (neznama->pParent != GetRoot())
+        {
+            neznama = neznama->pParent;
+            if (neznama->color == Color_t::BLACK)
+            {
+                blacks++;
+            }
+            else{}
+        }
+        blacks_in_tree[i] = blacks;
+    }
+    for (i = 0; i < sizeof(blacks_in_tree-1); i++)
+    {
+        EXPECT_TRUE(blacks_in_tree[i] == blacks_in_tree[i + 1]);
+    }
+}
+
+
+//============================================================================//
+// ** ZDE DOPLNTE TESTY **
+//
+// Zde doplnte testy Red-Black Tree, testujte nasledujici:
+// 1. Verejne rozhrani stromu
+//    - InsertNode/DeleteNode a FindNode
+//    - Chovani techto metod testuje pro prazdny i neprazdny strom.
+// 2. Axiomy (tedy vzdy platne vlastnosti) Red-Black Tree:
+//    - Vsechny listove uzly stromu jsou *VZDY* cerne.
+//    - Kazdy cerveny uzel muze mit *POUZE* cerne potomky.
+//    - Vsechny cesty od kazdeho listoveho uzlu ke koreni stromu obsahuji
+//      *STEJNY* pocet cernych uzlu.
+//============================================================================//
+
+/*** Konec souboru black_box_tests.cpp ***/
---
+++ BB,WB,TDD/tdd_code.cpp
@@ -0,0 +1,272 @@
+//======== Copyright (c) 2017, FIT VUT Brno, All rights reserved. ============//
+//
+// Purpose:     Test Driven Development - priority queue code
+//
+// $NoKeywords: $ivs_project_1 $tdd_code.cpp
+// $Author:     JMENO PRIJMENI <xlogin00@stud.fit.vutbr.cz>
+// $Date:       $2017-01-04
+//============================================================================//
+/**
+ * @file tdd_code.cpp
+ * @author JMENO PRIJMENI
+ * 
+ * @brief Implementace metod tridy prioritni fronty.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "tdd_code.h"
+
+//============================================================================//
+// ** ZDE DOPLNTE IMPLEMENTACI **
+//
+// Zde doplnte implementaci verejneho rozhrani prioritni fronty (Priority Queue)
+// 1. Verejne rozhrani fronty specifikovane v: tdd_code.h (sekce "public:")
+//    - Konstruktor (PriorityQueue()), Destruktor (~PriorityQueue())
+//    - Metody Insert/Remove/Find a GetHead
+//    - Pripadne vase metody definovane v tdd_code.h (sekce "protected:")
+//
+// Cilem je dosahnout plne funkcni implementace prioritni fronty implementovane
+// pomoci tzv. "double-linked list", ktera bude splnovat dodane testy 
+// (tdd_tests.cpp).
+//============================================================================//
+
+PriorityQueue::PriorityQueue()
+{
+    
+    item_first->pNext = NULL;
+    item_first->pPrev = NULL;
+    item_first->value = NULL;
+    
+    //item_first = NULL;
+}
+
+PriorityQueue::~PriorityQueue()
+{
+    Element_t* item = GetHead();
+    Element_t* item_help;
+    while (item != NULL)
+    {
+        item_help = item;
+        item = item->pNext;
+        delete item_help;
+    }
+}
+
+void PriorityQueue::Insert(int value)
+{
+
+
+    Element_t* item = GetHead();
+
+    if (item->pNext == NULL && item->pPrev == NULL && item->value == NULL)
+    {
+        item_first->value = value;
+    }
+    else
+    {
+        while (item != NULL)
+        {
+            if (item->pNext == NULL && item->pPrev == NULL)
+            {
+                if (item->value > value)
+                {
+                    //Insert to the FRONT
+                    Element_t* elmt = new Element_t;
+                    elmt->pNext = item;
+                    elmt->pPrev = NULL;
+                    elmt->value = value;
+                    item_first = elmt;
+                    item->pPrev = item_first;
+                }
+                else
+                {
+                    //Insert to the END
+                    Element_t* elmt = new Element_t;
+                    elmt->value = value;
+                    elmt->pPrev = item;
+                    elmt->pNext = NULL;
+                    item->pNext = elmt;
+                }
+                return;
+            }
+            else if (item->pPrev == NULL && item->pNext != NULL)
+            {
+                if (item->value > value)
+                {
+                    //Insert to the FRONT
+                    //Item value is bigger then Insert_Item.Value
+                    Element_t* elmt = new Element_t;
+                    elmt->pNext = item;
+                    elmt->pPrev = NULL;
+                    elmt->value = value;
+                    item_first = elmt;
+                    item->pPrev = item_first;
+                }
+                else
+                {/*
+                    Element_t* elmt = new Element_t;
+                    elmt->pNext = NULL;
+                    elmt->pPrev = item;
+                    elmt->value = value;
+                    item->pNext = elmt;*/
+                }
+                return;
+            }
+            else if (item->pPrev != NULL && item->pNext == NULL)
+            {
+                if (item->value > value)
+                {
+                    Element_t* elmt = new Element_t;
+                    elmt->value = value;
+                    elmt->pPrev = item->pPrev;
+                    elmt->pNext = item;
+                    item->pPrev->pNext = elmt;
+                    item->pPrev = item->pPrev->pNext;
+                }
+                else
+                {
+                    Element_t* elmt = new Element_t;
+                    elmt->value = value;
+                    elmt->pPrev = item;
+                    elmt->pNext = NULL;
+                    item->pNext = elmt;
+                }
+                return;
+            }
+            else if (item->pPrev != NULL && item->pNext != NULL)
+            {
+                //Middle item
+                if (item->pPrev->value <= value && item->value >= value)
+                {
+                    Element_t* elmt = new Element_t;
+                    elmt->value = value;
+                    elmt->pPrev = item->pPrev;
+                    elmt->pNext = item;
+                    item->pPrev->pNext = elmt;
+                    item->pPrev = elmt;
+                }
+                return;
+            }
+
+            item = item->pNext;
+        }
+    }
+
+    /*
+    Element_t* item = GetHead();
+
+    Element_t* elmt = new Element_t;
+    
+    if (item == NULL)
+    {
+        elmt->pNext = NULL;
+        elmt->pPrev = NULL;
+        elmt->value = value;
+        item_first = elmt;
+    }
+    else
+    {
+        if (item->pPrev == NULL && item->pNext == NULL)
+        {
+            if (item->value < value)
+            {
+                elmt->pNext = NULL;
+                elmt->pPrev = item_first;
+                elmt->value = value;
+                item_first->pNext = elmt;
+            }
+            else
+            {
+                elmt->pNext = item;
+                elmt->pPrev = NULL;
+                elmt->value = value;
+                item->pPrev = elmt;
+            }
+        }
+        else if (item->pNext != NULL)
+        {
+            
+            for (Element_t* item = GetHead(); value > item->value; item = item->pNext)
+                ;
+                       
+            if (item->pNext != NULL && item->pPrev != NULL)
+            {
+                elmt->pPrev = item->pNext->pPrev;
+                elmt->pNext = item->pPrev->pNext;
+                item->pNext->pPrev = elmt;
+                item->pPrev->pNext = elmt;
+                item = elmt;
+            }
+            else if (item->pNext == NULL && item->pPrev != NULL)
+            {
+                elmt->pNext = NULL;
+                elmt->pPrev = item->pPrev;
+                item = elmt;
+            }
+            else if (item->pNext != NULL && item->pPrev == NULL)
+            {
+                elmt->pPrev = NULL;
+                elmt->pNext = item_first;
+                item_first = elmt;                
+            }
+        }
+    }
+
+    */
+}
+
+bool PriorityQueue::Remove(int value)
+{
+    Element_t* item = Find(value);
+    
+    if (item != NULL)
+    {
+        if (item->pNext != NULL && item->pPrev != NULL)
+        {
+            item->pPrev->pNext = item->pNext;
+            item->pNext->pPrev = item->pPrev;
+            delete item;
+        }
+        else if (item->pNext != NULL && item->pPrev == NULL)
+        {
+            item->pNext->pPrev = NULL;
+            item_first = item->pNext;
+            delete item;
+        }
+        else if (item->pNext != NULL && item->pPrev == NULL)
+        {
+            item->pPrev->pNext = NULL;
+            delete item;
+        }
+        return true;
+    }
+    else 
+    {
+        return false;
+    }       
+    return false;
+}
+
+PriorityQueue::Element_t *PriorityQueue::Find(int value)
+{ 
+    Element_t* item = GetHead();
+    while (item != NULL)
+    {
+        if (item->value == value)
+        {
+            return item;
+        }
+        else{}
+        item = item->pNext;
+    }
+    return NULL;
+}
+
+PriorityQueue::Element_t *PriorityQueue::GetHead()
+{
+    return item_first;
+}
+
+/*** Konec souboru tdd_code.cpp ***/
